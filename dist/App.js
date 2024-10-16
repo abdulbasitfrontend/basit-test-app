1d75773e9b4dbba4304159ca1805c6576274fce2
Muhammad Abdul Basit Khan <105219703+abdulbasitfrontend@users.noreply.github.com>
2024-10-16 15:01:54 +0400

    commit

---
+++ dist/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}
+export default App;

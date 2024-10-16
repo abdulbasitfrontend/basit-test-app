d63155fa508c0e1a0e2d638180c14cf798469169
Muhammad Abdul Basit Khan <105219703+abdulbasitfrontend@users.noreply.github.com>
2024-10-16 16:46:24 +0400

    changes

---
+++ dist/Calculator.js
@@ -0,0 +1,6 @@
+function Calculator() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}
+export default Calculator;

7835d5421491337eb6035691abd8e560b1a4ddbd
Muhammad Abdul Basit Khan <105219703+abdulbasitfrontend@users.noreply.github.com>
2024-10-16 16:22:17 +0400

    changes

--- dist/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import Calculator from './Calculator';
-function App() {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "App"
-  }, /*#__PURE__*/React.createElement(Calculator, null));
-}
-export default App;
--- dist/Calculator.js
+++
@@ -1,6 +0,0 @@
-function Calculator() {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "App"
-  }, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
-}
-export default Calculator;
